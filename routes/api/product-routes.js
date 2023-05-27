919696e24791d86ab0b4f540b0df6f65091dc7cb
Potter <potteramylynn@gmail.com>
2023-05-27 15:10:50 -0500

    updated routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({ 
+      include:
+       [{ model: Category, through: Product, as: "category_id",
+        model: Tag, through: ProductTag, as: "product_tag" }] 
+      });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id { 
+      include:
+       [{ model: Category, through: Product, as: "category_id",
+        model: Tag, through: ProductTag, as: "product_tag" }] 
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // create new product
